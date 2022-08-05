66427fb4a44da010f8d34331fb6a9f295f46b932
wenodh <vinodkumara3@gmail.com>
2022-08-06 00:50:06 +0530

    cursor changed

--- src/pages/topBar/TopBar.jsx
+++ src/pages/topBar/TopBar.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './topBar.css';
 
+import { useNavigate } from 'react-router-dom';
+
 import CircleImg from '../../components/CircleImg';
 import logo from '../../images/NxtWave.png';
 
 const TopBar = () => {
+    let navigate = useNavigate();
     return (
         <div className="topBar">
-            <img className="logo" src={logo} alt="" />
+            <img
+                className="logo"
+                src={logo}
+                alt=""
+                onClick={() => navigate('/')}
+            />
             <CircleImg src={logo} size="40px" />
         </div>
     );
